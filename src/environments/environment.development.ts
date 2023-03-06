c0e34ee3b57b172a62c58ca1d9a6258c90e2ef1a
Shweta <shweta0agrawal98@gmail.com>
2023-03-07 00:48:54 +0530

    changes

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyA5IBuzu5b6su-Mly6Z-eY8rwtZ5bYTfG8",
+        authDomain: "oshop-216f2.firebaseapp.com",
+        projectId: "oshop-216f2",
+        storageBucket: "oshop-216f2.appspot.com",
+        messagingSenderId: "435971836157",
+        appId: "1:435971836157:web:d57e23578a2de36e3a1694",
+        measurementId: "G-R2JX51RFV5"
+    }
+};
